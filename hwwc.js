7e3e70cab7c9bf452d589596c04dca8146cd0cb5
Emlyn O'Regan <emlynoregan@gmail.com>
2021-07-17 01:57:31 +0000

    fix

--- hwwc.js
+++ hwwc.js
@@ -5,6 +5,9 @@ class HelloWorld extends HTMLElement {
         const shadow = this.attachShadow({ mode: "closed" });
         // create a span element
         const text = document.createElement("span");
+
+        const thing = this.thing();
+
         // set the content to 'Hello World'
         text.textContent = "Hello World - " + thing ? thing : "...no thing...";
         // insert our created element into our shadow DOM, causing it to appear
