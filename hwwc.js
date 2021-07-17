7e677e8d8f4f41cdc8215318c35203d932f5f619
Emlyn O'Regan <emlynoregan@gmail.com>
2021-07-17 00:40:21 +0000

    filled out hw component

--- hwwc.js
+++ hwwc.js
@@ -1,4 +1,15 @@
 class HelloWorld extends HTMLElement {
+    constructor() {
+        super();
+        // create a shadow dom
+        const shadow = this.attachShadow({ mode: "closed" });
+        // create a span element
+        const text = document.createElement("span");
+        // set the content to 'Hello World'
+        text.textContent = "Hello World";
+        // insert our created element into our shadow DOM, causing it to appear
+        shadow.appendChild(text);        
+    }
 }
 
 window.customElements.define('hello-world', HelloWorld);
