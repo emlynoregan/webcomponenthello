d2ce8b0555a0fd5745867e38bb0b234cb18d75d4
Emlyn O'Regan <emlynoregan@gmail.com>
2021-07-17 01:55:58 +0000

    added thing attribute

--- hwwc.js
+++ hwwc.js
@@ -6,9 +6,17 @@ class HelloWorld extends HTMLElement {
         // create a span element
         const text = document.createElement("span");
         // set the content to 'Hello World'
-        text.textContent = "Hello World";
+        text.textContent = "Hello World - " + thing ? thing : "...no thing...";
         // insert our created element into our shadow DOM, causing it to appear
         shadow.appendChild(text);        
+    }
+
+    get thing() {
+        return this.getAttribute('thing');
+    }
+      
+    set thing(newValue) {
+        this.setAttribute('thing', newValue);
     }
 }
 
