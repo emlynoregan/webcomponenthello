41e9ba54fdeac223685bfb0228679c23514f8104
Emlyn O'Regan <emlynoregan@gmail.com>
2021-07-17 02:03:40 +0000

    another fix

--- hwwc.js
+++ hwwc.js
@@ -9,7 +9,7 @@ class HelloWorld extends HTMLElement {
         const thing = this.thing;
 
         // set the content to 'Hello World'
-        text.textContent = "Hello World - " + thing ? thing : "...no thing...";
+        text.textContent = "Hello World - " + (thing ? thing : "...no thing...");
         // insert our created element into our shadow DOM, causing it to appear
         shadow.appendChild(text);        
     }
